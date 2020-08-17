9e9fef509eb57c4568291c75a720cf08f03a0ce3
georgematos <georgemattos@gmail.com>
2020-08-17 11:21:35 -0300

    completando o projeto usando a API do github

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GithubLogo from './github-logo.jpg';
 import './App.css';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.github.com/users/${search}`)
+      .then(resp => resp.json())
+      .then(userResp => setUserData(userResp));
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+
   return (
     <div className="container text-center">
       <h1 className="py-5">GitHub Profile</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <div className="input-group">
-            <input type="text" className="form-control" required />
+            <input
+              type="text"
+              className="form-control"
+              required
+              value={search}
+              onChange={handleChange} />
             <span className="input-group-btn">
               <button type="submit" className="btn btn-success">Search</button>
             </span>
@@ -17,21 +37,34 @@ function App() {
         </div>
       </form>
       <div className="py-5">
-        <img 
-          src={GithubLogo}
-          alt="logo do github"
-          className="responsive rounded-circle"
-          height="200p">
-        </img>
-        <h1 className="pt-5">
-          <a href="https://github.com/georgematos" target="_new">George Matos</a>
-        </h1>
-        <h3>Maracanaú</h3>
-        <p>
-          <a href="http://algumacoisa.com" target="_new" className="text-info">
-            http://algumacoisa.com
-          </a>
-        </p>
+        {!userData && (
+          <img
+            src={GithubLogo}
+            alt="logo do github"
+            className="responsive rounded-circle"
+            height="200p">
+          </img>
+        )}
+        {userData && (
+          <div>
+            <img
+              src={userData.avatar_url}
+              alt="logo do github"
+              className="responsive rounded-circle"
+              height="200p">
+            </img>
+            <h1 className="pt-5">
+              <a href={userData.html_url} target="_new">{userData.name}</a>
+            </h1>
+            <h3>{userData.location}</h3>
+            <p>
+              <a href={userData.blog} target="_new" className="text-info">
+              {userData.blog}
+              </a>
+            </p>
+            <span>{userData.bio}</span>
+          </div>
+        )}
       </div>
     </div>
   );
