a96eee7af1369eba79ba42993286382b8ec7ecd3
georgematos <georgemattos@gmail.com>
2020-08-17 10:25:13 -0300

    criacao da pagina estatica

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import GithubLogo from './github-logo.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container text-center">
+      <h1 className="py-5">GitHub Profile</h1>
+      <form>
+        <div className="form-group">
+          <div className="input-group">
+            <input type="text" className="form-control" required />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">Search</button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div className="py-5">
+        <img 
+          src={GithubLogo}
+          alt="logo do github"
+          className="responsive rounded-circle"
+          height="200p">
+        </img>
+        <h1 className="pt-5">
+          <a href="https://github.com/georgematos" target="_new">George Matos</a>
+        </h1>
+        <h3>Maracanaú</h3>
         <p>
-          Hello World!
+          <a href="http://algumacoisa.com" target="_new" className="text-info">
+            http://algumacoisa.com
+          </a>
         </p>
-      </header>
+      </div>
     </div>
   );
 }
